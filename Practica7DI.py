7776f36b8de9017244ad222531d781bd7f2ae50f
MiniMestri <fonsild97@hotmail.com>
2023-12-16 21:33:45 +0100

    Boton mismos datos
    Desarrollo de botón con get e insert para copiar los datos que están en un entry a otro

--- Practica7DI.py
+++ Practica7DI.py
@@ -1,13 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
 
-def deseleccionar(var,opciones):
-    for opcion in opciones:
-        if opcion != var:
-            opcion.set("")
 def copiar():
-    nombre2.delete(0,tk.END)
-    nombre2.insert(0,nombre.get())
+    entry_nombre_factura.insert(0,entry_nombre.get())
+    
+    entry_apellidos_factura.insert(0,entry_apellidos.get())
+
+    entry_direccion_factura.insert(0,entry_direccion.get())
+
+    entry_cp_factura.insert(0,entry_cp.get())
+
+    entry_provincia_factura.insert(0,entry_provincia.get())
+
+def confirmacion(evento):
+    ventana_confirmacion = tk.Toplevel(raiz, padx=50,pady=50)
+    ventana_confirmacion.title("Formulario de registro")
+
+    mensaje_confirmacion = ttk.Label(ventana_confirmacion,text="Registrado con éxito", font=('Arial',15))
+    mensaje_confirmacion.pack(padx=10,pady=10)
+
+    boton_cerrar = ttk.Button(ventana_confirmacion, text="Cerrar", command=ventana_confirmacion.destroy)
+    boton_cerrar.pack(pady=10)
 
 raiz=tk.Tk()
 
@@ -20,50 +33,67 @@ estilo.theme_use('clam')
 estilo.configure('TLabel',foreground='green',font=('Arial',15),background='white')
 estilo.configure('TRadiobutton',background='white')
 
+#DATOS DE ENVIO
 titulo = ttk.Label(raiz, text="DATOS DE ENVÍO").grid(row=0,column=1,padx=25,pady=10)
 
-#Nombre y cif
+#Introducción de nombre y apellidos
 ttk.Label(raiz, text="NOMBRE Y APELLIDOS").grid(row=1,column=0,padx=50,pady=10)
-nombre=ttk.Entry(raiz,width=30)
-nombre.grid(row=2,column=0,padx=25,pady=10)
-ttk.Label(raiz, text="CIF").grid(row=1,column=1,padx=25,pady=10)
-apellidos=ttk.Entry(raiz,width=30).grid(row=2,column=1,padx=25,pady=10)
+entry_nombre=ttk.Entry(raiz,width=30)
+entry_nombre.grid(row=2,column=0,padx=25,pady=10)
 
-#Direcciones
+ttk.Label(raiz, text="CIF").grid(row=1,column=1,padx=25,pady=10)
+entry_apellidos=ttk.Entry(raiz,width=30)
+entry_apellidos.grid(row=2,column=1,padx=25,pady=10)
+
+#Introducción de dirección de envío
 ttk.Label(raiz, text="DIRECCION").grid(row=3,column=0,padx=25,pady=30)
-direccion=ttk.Entry(raiz,width=30).grid(row=4,column=0,padx=25,pady=10)
+entry_direccion=ttk.Entry(raiz,width=30)
+entry_direccion.grid(row=4,column=0,padx=25,pady=10)
+
 ttk.Label(raiz, text="CÓDIGO POSTAL").grid(row=3,column=1,padx=25,pady=10)
-codigo=ttk.Entry(raiz,width=30).grid(row=4,column=1,padx=25,pady=10)
+entry_cp=ttk.Entry(raiz,width=30)
+entry_cp.grid(row=4,column=1,padx=25,pady=10)
+
 ttk.Label(raiz, text="PROVINCIA").grid(row=3,column=2,padx=25,pady=30)
-provincia=ttk.Entry(raiz,width=30).grid(row=4,column=2,padx=25,pady=10)
+entry_provincia=ttk.Entry(raiz,width=30)
+entry_provincia.grid(row=4,column=2,padx=25,pady=10)
 
 #Opcion de envio
-eleccion1=tk.StringVar()
-eleccion2=tk.StringVar()
-eleccion3=tk.StringVar()
+eleccion=tk.StringVar()
 
 ttk.Label(raiz, text="MÉTODO DE ENVÍO").grid(row=5,column=0,padx=25,pady=30)
-ttk.Radiobutton(raiz,text="Correos Express",variable=eleccion1,command=lambda:deseleccionar(eleccion1,[eleccion2,eleccion3])).grid(row=6,column=0,padx=25,pady=10)
-ttk.Radiobutton(raiz,text="MRW",variable=eleccion2,command=lambda:deseleccionar(eleccion2,[eleccion1,eleccion3])).grid(row=6,column=1,padx=10,pady=10)
-ttk.Radiobutton(raiz,text="SEUR",variable=eleccion3,command=lambda:deseleccionar(eleccion3,[eleccion1,eleccion2])).grid(row=6,column=2,padx=10,pady=10)                                                 
 
+ttk.Radiobutton(raiz,text="Correos Express",variable=eleccion,value="Correos Express").grid(row=6,column=0,padx=25,pady=10)
+ttk.Radiobutton(raiz,text="MRW",variable=eleccion,value="MRW").grid(row=6,column=1,padx=10,pady=10)
+ttk.Radiobutton(raiz,text="SEUR",variable=eleccion,value="SEUR").grid(row=6,column=2,padx=10,pady=10)                                                 
 
+#DATOS DE FACTURACIÓN
 titulo = ttk.Label(raiz, text="DATOS DE FACTURACIÓN").grid(row=7,column=1,padx=25,pady=10)
-ttk.Button(raiz,text="MISMOS DATOS",command=copiar).grid(row=7,column=2,padx=25,pady=10)
 
 #Nombre y cif
 ttk.Label(raiz, text="NOMBRE Y APELLIDOS").grid(row=8,column=0,padx=50,pady=10)
-nombre2=ttk.Entry(raiz,width=30)
-nombre2.grid(row=9,column=0,padx=25,pady=10)
+entry_nombre_factura=ttk.Entry(raiz,width=30)
+entry_nombre_factura.grid(row=9,column=0,padx=25,pady=10)
+
 ttk.Label(raiz, text="CIF").grid(row=8,column=1,padx=25,pady=10)
-apellidos2=ttk.Entry(raiz,width=30).grid(row=9,column=1,padx=25,pady=10)
+entry_apellidos_factura=ttk.Entry(raiz,width=30)
+entry_apellidos_factura.grid(row=9,column=1,padx=25,pady=10)
 
 #Direcciones
 ttk.Label(raiz, text="DIRECCION").grid(row=10,column=0,padx=25,pady=30)
-direccion=ttk.Entry(raiz,width=30).grid(row=11,column=0,padx=25,pady=10)
+entry_direccion_factura=ttk.Entry(raiz,width=30)
+entry_direccion_factura.grid(row=11,column=0,padx=25,pady=10)
+
 ttk.Label(raiz, text="CÓDIGO POSTAL").grid(row=10,column=1,padx=25,pady=10)
-codigo=ttk.Entry(raiz,width=30).grid(row=11,column=1,padx=25,pady=10)
+entry_cp_factura=ttk.Entry(raiz,width=30)
+entry_cp_factura.grid(row=11,column=1,padx=25,pady=10)
+
 ttk.Label(raiz, text="PROVINCIA").grid(row=10,column=2,padx=25,pady=30)
-provincia=ttk.Entry(raiz,width=30).grid(row=11,column=2,padx=25,pady=10)
+entry_provincia_factura=ttk.Entry(raiz,width=30)
+entry_provincia_factura.grid(row=11,column=2,padx=25,pady=10)
+
+#Botones
+boton=ttk.Button(raiz,text="MISMOS DATOS",command=copiar).grid(row=7,column=2,padx=25,pady=10)
+boton.bind("<Button-1>",confirmacion)
 
 raiz.mainloop()
